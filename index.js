77d683d421fc353fff964e859e989054103e2a1c
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-03-19 19:39:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('.navbar');   
+const logo = document.querySelector('.logo');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+        logo.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+        logo.classList.remove('scrolled');
+    }
+});
